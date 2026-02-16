8f6b23bef4e1542be68dd9f8ddc85f162799deef
abiola <vboxuser@localhost.localdomain>
2026-02-15 21:24:52 -0500

    files added to git

---
+++ aapp1/app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+redis_host = os.getenv("REDIS_HOST", "redis")
+
+redis = Redis(host=redis_host, port=6379)
+
+@app.route('/')
+def hello():
+    count = redis.incr('hits')
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
